0e6277a20286caeae4c67906cc908ff4512cff31
milosdjoric <milos.djoric@syncitgroup.com>
2026-01-29 15:09:37 +0100

    v0.0.9 changes for vercel #8

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -1,10 +1,8 @@
-import * as Headless from '@headlessui/react'
 import NextLink, { type LinkProps } from 'next/link'
 import React, { forwardRef } from 'react'
 
-export const Link = forwardRef(function Link(
-  props: LinkProps & React.ComponentPropsWithoutRef<'a'>,
-  ref: React.ForwardedRef<HTMLAnchorElement>
-) {
-  return <Headless.DataInteractive as={NextLink} {...props} ref={ref} />
-})
+export const Link = forwardRef<HTMLAnchorElement, LinkProps & React.ComponentPropsWithoutRef<'a'>>(
+  function Link(props, ref) {
+    return <NextLink {...props} ref={ref} />
+  }
+)
